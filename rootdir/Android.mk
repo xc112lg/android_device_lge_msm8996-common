9477504adc2756e825a969022a364e453e3b400e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-11-09 17:54:41 +0100

    msm8996-common: Switch to two-stage init mounting
    * Stop handling system mounting from kernel, first stage init is gonna
      do that
    * Use correct mount flags coming from kernel dts
    * Move system mount points to /dev/block/by-name/, we don't have
      bootdevice symlink to that point in init
    * Copy fstab to ramdisk for first stage init
    
    Change-Id: Iaa0ecc3f13c3cfd3fe357ca509549d0f68a29cf0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,8 +7,18 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
+LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
