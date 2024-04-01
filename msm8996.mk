078ed3124900b2b135a08ba5ddc0fdd8fb47b17e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-01 17:29:43 +0800

    Update msm8996.mk

--- msm8996.mk
+++ msm8996.mk
@@ -434,3 +434,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+
+
+# Speed up apps
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+   Settings \
+   SystemUI \
+   Launcher3QuickStep \
+   UnifiedSettingsApp
