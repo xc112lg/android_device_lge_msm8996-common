1f248e0495461969490ffc2d181c148d205e6e58
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-18 06:58:46 +0800

    save

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/Evolution-X-Devices/device_asus_sdm660-common.git -b tiramisu device/asus/sdm660-common
+git clone https://github.com/Evolution-X-Devices/vendor_asus_sdm660.git -b tiramisu vendor/asus
+git clone https://github.com/Evolution-X-Devices/kernel_asus_sdm660.git -b tiramisu kernel/asus/sdm660
+git clone https://gitlab.com/GhostMaster69-dev/cosmic-clang.git -b master prebuilts/clang/host/linux-x86/clang-cosmic
