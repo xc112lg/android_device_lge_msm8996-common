52fb20b14358263c7a99044b300afa626cbee112
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-19 20:02:37 +0800

    msm8996-common: overlay: Disable turbulence noise

--- msm8996.mk
+++ msm8996.mk
@@ -457,7 +457,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
 # UFFD GC
-OVERRIDE_ENABLE_UFFD_GC := false
+PRODUCT_ENABLE_UFFD_GC := true
 
 # Power
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
