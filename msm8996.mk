ea435ee8d719a3fda00b6dbaa370ed75a871f08a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-19 19:48:23 +0800

    msm8996-common: Disable UFFD GC via OVERRIDE_ENABLE_UFFD_GC

--- msm8996.mk
+++ msm8996.mk
@@ -455,3 +455,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
