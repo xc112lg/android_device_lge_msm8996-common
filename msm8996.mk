365e6fa09418d5f4294e86b2c94106f25322d94c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-19 19:56:38 +0800

    msm8996-common: Migrate QTI power HAL to soong_config_set

--- msm8996.mk
+++ msm8996.mk
@@ -458,3 +458,7 @@ PRODUCT_COPY_FILES += \
 
 # UFFD GC
 OVERRIDE_ENABLE_UFFD_GC := false
+
+# Power
+$(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
+$(call soong_config_set_bool,qtipower,interaction_boost,true)
