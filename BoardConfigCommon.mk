28939b0ab35ad77e957de61d65f81396923167d5
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-19 19:46:43 +0800

    msm8996-common: Use oops partition as metadata

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,3 +174,7 @@ WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Metadata
+BOARD_USES_METADATA_PARTITION := true
+
