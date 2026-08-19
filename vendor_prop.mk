bd59cb07cf7758b7273b0df4f59997cceb87483a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-19 20:10:08 +0800

    msm8996-common: Disable blurs during app launch

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -249,4 +249,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sf.blurs_are_expensive=1 \
 ro.surface_flinger.supports_background_blur=1 \
-persist.sys.sf.disable_blurs=1
+persist.sys.sf.disable_blurs=1 \
+ro.launcher.blur.appLaunch=0
