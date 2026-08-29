c66a4d4f0d0a0b5dcc63756a3763b657cf4b75ce
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-29 09:16:22 +0800

    dddd

--- msm8996.mk
+++ msm8996.mk
@@ -468,7 +468,3 @@ PRODUCT_ENABLE_UFFD_GC := false
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
-
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-platform.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-platform.xml
