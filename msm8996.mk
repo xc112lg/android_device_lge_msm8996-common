934272457f3189e8c78786291ce5a556273d1299
Nicholas Magill <nmagill1979@gmail.com>
2026-09-05 01:58:35 +0100

    msm8996-common: Drop custom audio service
    Change-Id: Ie8c61111217d8a5078ec7195b4a690afd51de623

--- msm8996.mk
+++ msm8996.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.service.lge_msm8996 \
+    android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     audio.primary.msm8996 \
