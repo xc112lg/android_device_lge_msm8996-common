2f9107e2de7275c902a9d6acc60522dc452e2ccc
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-13 15:59:34 +0800

    Remove system suspend package from build configuration
    Removed android.system.suspend package from PRODUCT_PACKAGES.

--- msm8996.mk
+++ msm8996.mk
@@ -382,9 +382,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/broadcom/libbt \
     hardware/lge
 
-# System Suspend
-PRODUCT_PACKAGES += \
-    android.system.suspend@1.0
 
 # Telephony
 PRODUCT_PACKAGES += \
